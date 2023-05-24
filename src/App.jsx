09d8002fc304316558b36d655faa6c183a65370d
fandok <fandika.okdiba@gmail.com>
2023-05-24 20:53:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Bar, Line } from "react-chartjs-2";
+
+const labels = ["Januari", "Februari", "Maret", "April", "Mei", "Juni"];
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "My First Dataset",
+      backgroundColor: "rgb(255,99,132)",
+      borderColor: "rgb(255,99,132",
+      data: [0, 10, 5, 2, 20, 30],
+    },
+  ],
+};
+
+const App = () => {
+  return (
+    <div>
+      <Line data={data} />
+      <Bar
+        style={{ width: 1000 }}
+        options={{
+          plugins: { legend: { display: false } },
+          scales: {
+            y: { title: { display: true, text: "Amount of Car Rentals" } },
+            x: { title: { display: true, text: "Date" } },
+          },
+        }}
+        data={{
+          labels: Array.from({ length: 30 }, (_, i) => i + 1),
+          datasets: [
+            {
+              data: Array.from({ length: 30 }, () => Math.random() * 100),
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
